20bacdb1bbe90e0f95aed333441d7baa48750dd0
badenk4271 <ayavarav@gmail.com>
2017-10-25 22:03:21 -0300

    aca se creo entradas de datos

---
+++ src/proyecto1alejandro/Test1.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1alejandro;
+
+/**
+ *
+ * @author LAB01
+ */
+public class Test1
+{
+  public static void main(String []args) // esto es una funcion o metodo
+  {
+      int numero;//Declaracion de variable tipo entero
+      double numero2;//Declaracion tipo double
+      String name;// Aqui guardamos texto y String siempre va con mayuscula
+      float decimal;// Declaramos variables tipo decimas
+     boolean respuesta = true;//Declaracion e inicializacion
+     
+      numero=60;//Acabo de inicializar la variable numero 60
+      numero2=3.1564564654;//Inicializamos la variable numero2 
+       name="Alejandro Yavara";//asignamos un valor a la variable name  
+       decimal=3.15f;// Agrego literal f (float) para evitar perdida de presicion Aqui va un valor variable decimal tipo float
+       
+     //En este bloque vamos a usar las variables
+     System.out.println("Suma de numero y numero2:"+(numero+numero2));
+     System.out.println("Bienvenido \t"+name);
+       
+              
+      
+      
+  }
+  
+}
+  
